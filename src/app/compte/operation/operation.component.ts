1eb41aa644a8f5611a77c47c03785d2f21b8e142
Marko1212 <shihonage7@gmail.com>
2020-07-24 19:27:17 +0200

    transactions

--- src/app/compte/operation/operation.component.ts
+++ src/app/compte/operation/operation.component.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { OperationService } from './operation.service';
-import { Operation } from './operation';
-import { ActivatedRoute } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { OperationService } from "./operation.service";
+import { Operation } from "./operation";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-operation',
-  templateUrl: './operation.component.html',
-  styleUrls: ['./operation.component.css']
+  selector: "app-operation",
+  templateUrl: "./operation.component.html",
+  styleUrls: ["./operation.component.css"],
 })
 export class OperationComponent implements OnInit {
   operation: Operation;
   id: number;
-  constructor(private operationService: OperationService, private route: ActivatedRoute) {
-    this.operation = new Operation('',null,new Date(),null,'',null,'');
+  constructor(
+    private operationService: OperationService,
+    private route: ActivatedRoute
+  ) {
+    this.operation = new Operation(
+      "",
+      null,
+      new Date(),
+      null,
+      "",
+      null,
+      "",
+      null
+    );
   }
 
   ngOnInit() {
-   this.id = this.route.snapshot.params.id;
-   console.log(this.id );
+    this.id = this.route.snapshot.params.id;
+    console.log(this.id);
   }
-  createPayment(){
-  this.operationService.sendOperation(this.operation, this.id).subscribe(result => console.log(result));
-}
+  createPayment() {
+    this.operationService
+      .sendOperation(this.operation, this.id)
+      .subscribe((result) => console.log(result));
+  }
 }
--- src/app/compte/operation/operation.ts
+++ src/app/compte/operation/operation.ts
@@ -1,7 +1,12 @@
 export class Operation {
-    constructor(public transactionType: string, public numero_transaction: number,
-                public date_creation: Date, public montant: number, public typeMvt: string,
-                public dateMvt: Date, public libelle: string) { }
-
-
+  constructor(
+    public transactionType: string,
+    public numero_transaction: number,
+    public date_creation: Date,
+    public montant: number,
+    public typeMvt: string,
+    public dateMvt: Date,
+    public libelle: string,
+    public iban: number
+  ) {}
 }
