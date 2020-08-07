2150a3ec69b358a015b4db1a8841931527e7ffa9
Marko1212 <shihonage7@gmail.com>
2020-08-07 14:26:48 +0200

    update

--- src/app/client/list-client/list-client.component.ts
+++ src/app/client/list-client/list-client.component.ts
@@ -49,4 +49,22 @@ export class ListClientComponent implements OnInit {
         }
       });
   }
+
+  createChequeBookForBankAccount(compte) {
+    //TODO
+    //Cekovna knjiza se kreira za odredjeni racun, current ili saving, i nema ogranicenja u broju tih knjizica.
+    //Posle klika na dugme, klijent dobija mejl da mu je odobren rikvest za cekovnu knjizicu i da ce mu
+    //ista biti poslata postom na kucnu adresu.
+    //Proveriti da li je bank Account valid prilikom kreiranja cekovne knjizice za racun.
+    //To dugme treba da postoji u svakom redu, ukljucujuci i saving account.
+  }
+
+  createSavingAccountForClient(compte) {
+    //TODO
+    //Saving account moze da bude samo jedan za klijenta, znaci, dugme za kreiranje ce da "posivi" posle klika
+    //kreiranja saving account racuna. Proveriti da li je klijent validan prilikom kreiranja tog racuna. Dugme treba da bude "sivo" (t.j.neaktivno) jedino ako je saving account
+    //racun validan. Ukoliko agent deaktivira saving account (znaci account vise nije validan), dugme treba ponovo da postane aktivno.
+    //Posle klika na dugme, klijent dobija mejl da mu je odobren rikvest i da mu je kreiran saving account racun.
+    //Saving account racun treba da se pojavi u listi racuna, sa dugmicima: Show, Deactivate i Create Cheque Book for Bank Account.
+  }
 }
